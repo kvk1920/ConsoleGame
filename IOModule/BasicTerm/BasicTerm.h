380b598696969848f0ffb955e929fdd87d948a2e
kvk1920 <kvk1901@yandex.ru>
2018-04-04 04:47:29 +0300

    Reorganised structure of BaseTerm

---
+++ IOModule/BasicTerm/BasicTerm.h
@@ -0,0 +1,5 @@
+//Use this function whe you start to work with terminal
+//ATTENTION: in this mode terminal doesn't print chars that you type!!!
+inline void __terminal_set_keypress(void);
+//Use this function to switch terminal to usual mode.
+inline void __terminal_reset_key_press(void);
